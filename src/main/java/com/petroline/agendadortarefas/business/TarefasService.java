c9c8299fd7d46b0b7b9acdaf1d4bdc661b3f7ba5
Everton <evertonpetroline@outlook.com>
2025-11-13 06:21:39 -0300

    adicionado metodos Get por email e por periodo

--- src/main/java/com/petroline/agendadortarefas/business/TarefasService.java
+++ src/main/java/com/petroline/agendadortarefas/business/TarefasService.java
@@ -1,7 +1,7 @@
 package com.petroline.agendadortarefas.business;
 
 import com.petroline.agendadortarefas.business.dto.TarefasDTO;
-import com.petroline.agendadortarefas.business.mapper.TarefaConverter;
+import com.petroline.agendadortarefas.business.mapper.TarefasConverter;
 import com.petroline.agendadortarefas.infrastructure.entity.TarefasEntity;
 import com.petroline.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
 import com.petroline.agendadortarefas.infrastructure.repository.TarefasRepository;
@@ -10,18 +10,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TarefasService {
 
     private final TarefasRepository tarefasRepository;
-    private final TarefaConverter tarefaConverter;
+    private final TarefasConverter tarefaConverter;
     private final JwtUtil jwtUtil;
 
 
-    public TarefasDTO gravarTarefa(String token, TarefasDTO dto){
-        String email = jwtUtil.extrairEmailToken(token.substring( 7));
+    public TarefasDTO gravarTarefa(String token, TarefasDTO dto) {
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
         dto.setDataCriacao(LocalDateTime.now());
         dto.setStatusNotificacaoEnum(StatusNotificacaoEnum.PENDENTE);
         dto.setEmailUsuario(email);
@@ -30,4 +31,20 @@ public class TarefasService {
         return tarefaConverter.paraTarefaDTO(
                 tarefasRepository.save(entity));
     }
+
+    public List<TarefasDTO> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal) {
+
+        return tarefaConverter.paraListaTarefasDTO(
+                tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal));
+
+    }
+
+    public List<TarefasDTO> buscaTarefasPorEmail(String token){
+
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+        List<TarefasEntity> listaTarefas = tarefasRepository.findByEmailUsuario(email);
+
+        return tarefaConverter.paraListaTarefasDTO(listaTarefas);
+    }
+
 }
